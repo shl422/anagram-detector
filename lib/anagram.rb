b8264ca2ac2d801b94e8f9cd758ec1d79c5053e4
Shell <h.k.domingo@outlook.com>
2022-05-03 13:04:08 +0800

    “Done”

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+      @word = word
+    end
+  
+  def match(array)
+   words_sort = array.map { |item| item.chars.sort.join}
+   match = @word.chars.sort.join
+   matches = []
+   words_sort.each_with_index do |word, index|
+     if word=== match
+       matches<<array[index]
+     end
+  end
+  matches
+  end
+  end
